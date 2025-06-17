fd0adb824702a2556a48610975255bfb5ab2c75a
marcos souza <marcos.hsouza@senacsp.edu.br>
2025-06-16 22:17:11 -0300

    adição de categoria

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
 
+<<<<<<< HEAD
 app.use(express.urlencoded({ extended: true }));
+=======
+app.use(express.urlencoded({extended: true}));
+>>>>>>> 17958e1 (adição de categoria)
 
 const mysql = require('mysql2');
 
@@ -57,6 +61,7 @@ app.get('/produtos/add', (req, res) => {
   });
 });
 
+<<<<<<< HEAD
 app.post('/produtos/add', (req, res) => {
   const { nome, descricao, preco, estoque, categoria_id } = req.body;
 
@@ -78,11 +83,36 @@ app.post('/produtos/add', (req, res) => {
 app.get('/categorias', (req, res) => {
   let sql = 'SELECT * FROM categorias';
   conexao.query(sql, function (erro, categorias_qs) {
+=======
+app.post('/produtos/add', (req,res) => {
+  const {nome, descricao, preco, estoque, categoria_id} = req.body; //este body define a forma de transmissão de pacotes
+
+  const sql = `
+    INSERT INTO produtos (nome, descricao, preco, estoque,categoria_id) VALUES (?,?,?,?,?)
+`;
+  conexao.query(sql, [nome,descricao, preco, estoque, categoria_id], (erro, resultado) => {
+    if (erro) {
+      console.error (' ❌ erro ao inserir produto:', erro);
+      return res.status (500).send (' ❌ erro ao adicionar produto');
+    }
+
+    res. redirect('/'); // exibe na raiz
+  });
+});
+
+// ---inserir novo botão de inserir categoria
+
+app.get('/categoria/add', (req, res) => {
+  let sql = 'SELECT id, nome FROM categoria';
+  
+  conexao.query(sql, function (erro, categoria_qs) {
+>>>>>>> 17958e1 (adição de categoria)
     if (erro) {
       console.error('😫 Erro ao consultar categorias:', erro);
       res.status(500).send('Erro ao consultar categorias');
       return;
     }
+<<<<<<< HEAD
     res.render('categorias', { categorias: categorias_qs });
   });
 });
@@ -102,6 +132,31 @@ app.post('/categoria/add', (req, res) => {
     res.redirect('/categorias');
   });
 });   
+=======
+
+    res.render('produto_form', { categoria: categoria_qs });
+  });
+});
+
+app.post('/categoria/add', (req,res) => {
+  const {nome, descricao } = req.body; //este body define a forma de transmissão de pacotes
+
+  const sql = `
+    INSERT INTO produtos (nome, descricao) VALUES (?,?)
+`;
+  conexao.query(sql, [nome,descricao], (erro, resultado) => {
+    if (erro) {
+      console.error (' ❌ erro ao inserir produto:', erro);
+      return res.status (500).send (' ❌ erro ao adicionar produto');
+    }
+
+    res. redirect('/'); // exibe na raiz
+  });
+});
+
+
+//---------
+>>>>>>> 17958e1 (adição de categoria)
 
 
 app.get('/clientes', (req, res) => {;
